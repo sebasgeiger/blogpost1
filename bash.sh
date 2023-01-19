9e29013760443c225f6ad263d69ba55d69e2e02b
Sebastian Geiger <geigersr@clarkson.edu>
2023-01-19 15:25:50 -0500

    add bash and md files

---
+++ bash.sh
@@ -0,0 +1,20 @@
+cd "/mnt/c/Users/sgeig/OneDrive - clarkson.edu/Desktop/8th Semester/COMM429"
+
+echo Please Enter Project folder name
+read foldername
+
+mkdir $foldername
+cd $foldername
+mkdir css js img
+touch index.html
+
+cd css 
+touch style.css
+
+cd ..
+cd js
+touch page.js
+
+cd ..
+
+
---
+++ sample.md
@@ -0,0 +1,3 @@
+# Blog Post 1
+
+
