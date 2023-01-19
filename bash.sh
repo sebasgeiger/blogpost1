c3b62cd8ddb851a2645b0d390c0154e6317b14d6
Sebastian Geiger <geigersr@clarkson.edu>
2023-01-19 17:14:05 -0500

    added html and css code

--- bash.sh
+++ bash.sh
@@ -5,16 +5,72 @@ read foldername
 
 mkdir $foldername
 cd $foldername
+
 mkdir css js img
 touch index.html
 
 cd css 
 touch style.css
+touch normalize.css
 
 cd ..
 cd js
 touch page.js
 
 cd ..
+
+echo "<!DOCTYPE html>" >> index.html
+echo "<html lang=\"en-US\">" >> index.html
+echo "<head>" >> index.html
+echo "    <meta charset=\"utf-8\">" >> index.html
+echo "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">" >> index.html
+echo "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" >> index.html
+echo "    <title></title>" >> index.html
+echo "    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">" >> index.html
+echo "    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>" >> index.html
+echo "    <link href=\"https://fonts.googleapis.com/css2?family=Gentium+Book+Basic:wght@400;700&family=Lato:wght@400;700&display=swap\" rel=\"stylesheet\">" >> index.html
+echo "    <link href=\"css/modern-normalize.css\" rel=\"stylesheet\">" >> index.html
+echo "    <link href=\"css/style.css\" rel=\"stylesheet\">" >> index.html
+echo "    <script src=\"https://kit.fontawesome.com/b03388ca7d.js\" crossorigin=\"anonymous\"></script>" >> index.html
+echo "</head>" >> index.html
+echo "<body>" >> index.html 
+echo "    <h1>If blue, html and style sheets are working<h1>" >> index.html
+echo "</body>" >> index.html
+
+echo "h1 {color: blue;}" >> css/style.css
+
+echo "::before,::after {box-sizing: border-box;}" >> css/normalize.css
+echo "html {-moz-tab-size: 4;tab-size: 4;}" >> css/normalize.css
+echo "html {line-height: 1.15; -webkit-text-size-adjust: 100%;}" >> css/normalize.css
+echo "body {margin: 0;}" >> css/normalize.css
+echo "body {font-family:system-ui, apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji';}" >> css/normalize.css
+echo "hr {height: 0; color: inherit; }" >> css/normalize.css
+echo "b,strong {font-weight: bolder;}" >> css/normalize.css
+echo "code,kbd,samp,pre {font-family:ui-monospace,SFMono-Regular,Consolas,'Liberation Mono',Menlo,monospace; font-size: 1em; }" >> css/normalize.css
+echo "small {font-size: 80%;}" >> css/normalize.css
+echo "sub,sup {font-size: 75%; line-height: 0;position: relative; vertical-align: baseline;}" >> css/normalize.css
+echo "sub {bottom: -0.25em;}" >> css/normalize.css
+echo "sup {top: -0.5em;}" >> css/normalize.css
+echo "table {text-indent: 0; border-color: inherit;}" >> css/normalize.css
+echo "button,input,optgroup,select,textarea {font-family: inherit; font-size: 100%; line-height: 1.15; margin: 0;}" >> css/normalize.css
+echo "button,select {text-transform: none;}" >> css/normalize.css
+echo "button,[type='button'],[type='reset'],[type='submit'] {-webkit-appearance: button;}" >> css/normalize.css
+echo "::-moz-focus-inner {border-style: none;padding: 0;}" >> css/normalize.css
+echo ":-moz-focusring {outline: 1px dotted ButtonText;}" >> css/normalize.css
+echo ":-moz-ui-invalid {box-shadow: none;}" >> css/normalize.css
+echo "legend {padding: 0;}" >> css/normalize.css
+echo "progress {vertical-align: baseline;}" >> css/normalize.css
+echo "::-webkit-inner-spin-button,::-webkit-outer-spin-button {height: auto;}" >> css/normalize.css
+echo "[type='search'] {-webkit-appearance: textfield; outline-offset: -2px;}" >> css/normalize.css
+echo "::-webkit-search-decoration {-webkit-appearance: none;}" >> css/normalize.css
+echo "::-webkit-file-upload-button {-webkit-appearance: button; font: inherit;}" >> css/normalize.css
+echo "summary {display: list-item;}" >> css/normalize.css 
+
+
+
+
+
+
+
 
 
--- sample.md
+++ sample.md
@@ -1,3 +1,43 @@
 # Blog Post 1
 
+The purpose for this blog was to write a command-line script that created a boilerplate for a project starter. This script will create a project folder with the personal file and folder setup I perfer inside that folder. It also provides some standard code in some files that appear in every project. 
 
+## Step 1
+
+The first step for me was to set the directory I want my projects to go at the top of my script. I chose the folder I use for this class.
+```
+cd "/mnt/c/Users/sgeig/OneDrive - clarkson.edu/Desktop/8th Semester/COMM429"
+```
+I used quotes around the directory because of the spaces I have in my directory. The quotes prevent any errors occuring with the terminal thinking that the spaces mean that there are multiple arguements. 
+
+## Step 2
+Next I created an input statement for the user to a name for the project folder. The script takes this input as a variable, it reads in this variable and uses it as the name of the folder, then sets the directory to this folder.
+```
+echo Please Enter Project folder name
+read foldername
+mkdir $foldername
+cd $foldername
+```
+foldername is the name of the variable that I read in from the user and use to create a folder and set the new directory.
+
+## Step 3
+Once I have my project folder created I want to add the folders and files I need inside that folder. With the directory set I can write commands to add folders and files. I added a css,js, and img folder as well as a index.html file.
+```
+mkdir css js img
+touch index.html
+```
+
+## Step 4
+My next step was to create the docs for each of the folders in my project folder. The **css** folder will contain my css style sheets, **js** folder will contain my javascript files, and the **img** folder will contain any images I use in my project.
+
+To do this I changed the directory to one of these folders then created a file. Then I backuped my directory one step and repeated this process. I only needed to add files to the **css** and **js** folders, because every project will use at least a little bit of the same css and javascript code, but the images will change every project. 
+```
+cd css 
+touch style.css
+
+cd ..
+cd js
+touch page.js
+
+cd ..
+```
